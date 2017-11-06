a4eef24975dd2591bd9d4a5a8f5553823dc37d09
Marcin Przewięźlikowski <m.przewie@gmail.com>
2017-11-06 15:52:56 +0100

    measuring the performance of model

--- 2017_11_06_lab3/solutions.py
+++ 2017_11_06_lab3/solutions.py
@@ -4,6 +4,7 @@ import numpy as np
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+
 def _hypotheses(W, X):
     result = X.dot(W)
     return sigmoid(result)
@@ -11,7 +12,7 @@ def _hypotheses(W, X):
 
 def cost(W, X, Y, reg_param=0.1, eps=0.01):
     hypotheses = _hypotheses(W, X)
-    result = Y * np.log(hypotheses  + eps) + (1 - Y) * np.log(1 - hypotheses + eps)
+    result = Y * np.log(hypotheses + eps) + (1 - Y) * np.log(1 - hypotheses + eps)
     result = result.mean()
     result *= -1
     # print(result)
@@ -38,6 +39,42 @@ def mean_normalization(feature_matrix):
     return (feature_matrix - means) / ranges
 
 
-def secret_polynomial(x):
-    return 0.5 * x ** 3 - 4*x + 6 * np.random.rand()
+def accuracy(actual_predictions, model_predictions):
+    equals = (actual_predictions == model_predictions).astype(int)
+    return equals.sum() / equals.size
 
+
+def precision(actual_predictions, model_predictions):
+    equals = actual_predictions == model_predictions
+    eq = equals.astype(int)
+    tp = actual_predictions * eq
+    neq = (equals == False).astype(int)
+    fp = neq * model_predictions
+    return tp.sum() / (tp.sum() + fp.sum())
+
+
+def recall(actual_predictions, model_predictions):
+    equals = actual_predictions == model_predictions
+    eq = equals.astype(int)
+    tp = actual_predictions * eq
+    neq = (equals == False).astype(int)
+    fn = neq * (model_predictions == 0).astype(int)
+    return tp.sum() / (tp.sum() + fn.sum())
+
+
+def f_score(actual_predictions, model_predictions):
+    p = precision(actual_predictions, model_predictions)
+    r = recall(actual_predictions, model_predictions)
+    return (2 * p * r) / (p + r)
+
+
+tpr = recall
+
+
+def fpr(actual_predictions, model_predictions):
+    equals = actual_predictions == model_predictions
+    eq = equals.astype(int)
+    tn = (actual_predictions == 0).astype(int) * eq
+    neq = (equals == False).astype(int)
+    fp = neq * model_predictions
+    return fp.sum() / (fp.sum() + tn.sum())
