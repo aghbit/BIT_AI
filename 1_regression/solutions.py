5076e48de78002ac544a3cc6b277a84d4f4e9fcf
Marcin Przewięźlikowski <m.przewie@gmail.com>
2018-07-21 16:40:30 +0200

    basic regression examples done

--- 1_regression/solutions.py
+++ 1_regression/solutions.py
@@ -1,6 +1,7 @@
 import numpy as np
+from typing import Tuple, List
 
-def my_loss(w_0: float, w_1: float, X: np.ndarray, Y: np.ndarray):
+def my_loss(w_0: float, w_1: float, X: np.ndarray, Y: np.ndarray) -> float:
     total_error = 0.0
     N = len(X)
     for curr_x, curr_y in zip(X, Y):
@@ -10,31 +11,37 @@ def my_loss(w_0: float, w_1: float, X: np.ndarray, Y: np.ndarray):
     loss = total_error / N
     return loss
 
-def my_loss_vectorized(w_0: float, w_1: float, X: np.ndarray, Y: np.ndarray):
+def my_loss_vectorized(w_0: float, w_1: float, X: np.ndarray, Y: np.ndarray) -> float:
     Y_pred = w_0 + w_1 * X
-    errors = (Y - Y_pred) ** 2
+    errors = (Y_pred - Y) ** 2
     loss = errors.mean()
     return loss    
 
-def dLdw0(w_0: float, w_1: float, X: np.ndarray, Y: np.ndarray):
+def dLdw_0(w_0: float, w_1: float, X: np.ndarray, Y: np.ndarray) -> float:
     Y_pred = w_0 + w_1 * X
-    errors = (Y - Y_pred)
-    return errors.mean()
+    errors = Y_pred - Y
+    return errors.mean() * 2
 
-def dLdw1(w_0: float, w_1: float, X: np.ndarray, Y: np.ndarray):
+def dLdw_1(w_0: float, w_1: float, X: np.ndarray, Y: np.ndarray) -> float:
     Y_pred = w_0 + w_1 * X
-    errors = (Y - Y_pred)
+    errors = Y_pred - Y
     errors_mul = errors * X
-    return errors_mul.mean()
+    return errors_mul.mean() * 2
 
-def gradient_step(x, y, w_0, w_1, alpha=0.01):
-    # TO BE IMPLEMENTED
-    N = len(x)
+def gradient_step_naive(
+    w_0: float, 
+    w_1: float, 
+    X: np.ndarray, 
+    Y: np.ndarray, 
+    learning_rate: float
+) -> Tuple[float, float, float]:
+    loss = my_loss(w_0, w_1, X, Y)
+    N = len(X)
     
     delta_w_1 = 0.0
     delta_w_0 = 0.0
     
-    for curr_x, curr_y in zip(x, y):
+    for curr_x, curr_y in zip(X, Y):
         y_pred = w_1 * curr_x + w_0 
         error =  y_pred - curr_y
         delta_w_1 += error * curr_x
@@ -42,16 +49,41 @@ def gradient_step(x, y, w_0, w_1, alpha=0.01):
         
     delta_w_1 *= (2/N)
     delta_w_0 *= (2/N)
-    w_1 = w_1 - alpha * delta_w_1
-    w_0 = w_0 - alpha * delta_w_0
-    
-    return w_0, w_1
+    w_1 = w_1 - learning_rate * delta_w_1
+    w_0 = w_0 - learning_rate * delta_w_0
+    return w_0, w_1, loss
 
+def gradient_step_vectorized(
+    w_0: float, 
+    w_1: float, 
+    X: np.ndarray, 
+    Y: np.ndarray, 
+    learning_rate: float
+) -> Tuple[float, float, float]:
+    loss = my_loss_vectorized(w_0, w_1, X, Y)
+    delta_w_0 = dLdw_0(w_0, w_1, X, Y) * learning_rate
+    delta_w_1 = dLdw_1(w_0, w_1, X, Y) * learning_rate
+    w_0 = w_0 - delta_w_0
+    w_1 = w_1 - delta_w_1
+    return w_0, w_1, loss
 
+def train_model(
+    init_w_0: float,
+    init_w_1: float,
+    X: np.ndarray,
+    Y: np.ndarray,
+    learning_rate: float,
+    num_iterations: int
+) -> Tuple[float, float, List[float]]:
+    w_0 = init_w_0
+    w_1 = init_w_1
+    loss_history = [] 
+    for i in range(num_iterations):
+        w_0, w_1, loss = gradient_step_vectorized(w_0, w_1, X, Y, learning_rate)
+        loss_history.append(loss)
+    return w_0, w_1, loss_history
 
-x = np.linspace(1, 10)
-noise = np.random.normal(size=x.shape)
-y = 2 * x + 1 + noise 
-
-y_hard = 10 * np.sin(x) + 5 * x - 10 + 3 * noise
+X_hard = np.linspace(1, 10)
+noise = np.random.normal(size=X_hard.shape)
+Y_hard = 10 * np.sin(X_hard) + 5 * X_hard - 10 + 3 * noise
 
